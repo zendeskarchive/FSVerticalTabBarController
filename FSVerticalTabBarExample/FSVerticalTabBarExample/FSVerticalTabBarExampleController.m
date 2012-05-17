b4000a3a61c2f8e51eb82fa3e39b48fc9e82d8ee
Natalia <Natalia.Terlecka@gmail.com>
2012-05-17 22:25:48 +0200

    Reparied bug that caused first viewController to remove itself before loading.

--- FSVerticalTabBarExample/FSVerticalTabBarExample/FSVerticalTabBarExampleController.m
+++ FSVerticalTabBarExample/FSVerticalTabBarExample/FSVerticalTabBarExampleController.m
@@ -18,23 +18,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+        
+    //create view controllers
+    UIViewController *viewRed = [[UIViewController alloc] init];
+    UIViewController *viewBlue = [[UIViewController alloc] init];
+    UIViewController *viewGreen = [[UIViewController alloc] init];
+    UIViewController *viewBrown = [[UIViewController alloc] init];
+    UIViewController *viewYellow = [[UIViewController alloc] init];
+    UIViewController *viewWhite = [[UIViewController alloc] init];
     
-    //Create the first view controller
-    UIViewController * viewRed = [[UIViewController alloc] init];
-    
-    //Create tabItem for the view controller
-    viewRed.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 1" image:[UIImage imageNamed:@"magnifying-glass.png"] tag: 0];
+    //change background colors
+    [viewBlue.view setBackgroundColor:[UIColor blueColor]];
+    [viewGreen.view setBackgroundColor:[UIColor greenColor]];
+    [viewBrown.view setBackgroundColor:[UIColor brownColor]];
+    [viewYellow.view setBackgroundColor:[UIColor yellowColor]];
+    [viewWhite.view setBackgroundColor:[UIColor whiteColor]];
     [viewRed.view setBackgroundColor:[UIColor redColor]];
 
-    //create another view controller
-    UIViewController *viewBlue = [[UIViewController alloc] init];
-    
-    //change background color of second view to differentiate
-    [viewBlue.view setBackgroundColor:[UIColor blueColor]];
+    //Create tabItems for the view controller
+    viewRed.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 1" image:[UIImage imageNamed:@"magnifying-glass.png"] tag: 0];
     viewBlue.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 2" image:[UIImage imageNamed:@"magnifying-glass.png"] tag:1];
+    viewGreen.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 3" image:[UIImage imageNamed:@"magnifying-glass.png"] tag:2];
+    viewBrown.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 4" image:[UIImage imageNamed:@"magnifying-glass.png"] tag:3];
+    viewYellow.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 5" image:[UIImage imageNamed:@"magnifying-glass.png"] tag:4];
+    viewWhite.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"tab 5" image:[UIImage imageNamed:@"magnifying-glass.png"] tag:5];
     
     //set the view controllers of the the tab bar controller
-    [self setViewControllers:[NSArray arrayWithObjects:viewRed,viewBlue, nil]];
+    [self setViewControllers:[NSArray arrayWithObjects:viewRed,viewBlue,viewGreen,viewBrown,viewYellow,viewWhite, nil]];
         
     //set the background color to a texture
     [[self tabBar] setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"ios-linen.png"]]];
